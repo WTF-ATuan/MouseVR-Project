f3cf9e173fce6ba3f364477da5fc979ddcb43c6c
ATuan <4A8K0069@stust.edu.tw>
2022-02-24 14:52:44 +0800

    added Actor Edior

---
+++ Assets/Actor/Editor/ActorEditor.cs
@@ -0,0 +1,17 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Actor.Editor{
+	public class ActorEditor : EditorWindow{
+		[MenuItem("Tools/Project/ActorEditor")]
+		private static void ShowWindow(){
+			var window = GetWindow<ActorEditor>();
+			window.titleContent = new GUIContent("TITLE");
+			window.Show();
+		}
+
+		private void OnGUI(){ }
+	}
+}
+#endif
