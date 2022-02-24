00e5fb284942420e451e514d9b3a8cd11078f72e
ATuan <4A8K0069@stust.edu.tw>
2022-02-24 16:03:27 +0800

    added receive reward button and Teleport button

--- Assets/Actor/Editor/ActorEditor.cs
+++ Assets/Actor/Editor/ActorEditor.cs
@@ -1,8 +1,10 @@
 ﻿#if UNITY_EDITOR
+using System;
 using Sirenix.OdinInspector.Editor;
 using Sirenix.Utilities;
 using Sirenix.Utilities.Editor;
 using UnityEditor;
+using UnityEngine;
 
 namespace Actor.Editor{
 	public class ActorEditor : OdinEditorWindow{
@@ -13,6 +15,55 @@ namespace Actor.Editor{
 			window.Show();
 		}
 
+		private Scripts.Actor actor;
+
+		protected override void OnEnable(){
+			actor = FindObjectOfType<Scripts.Actor>();
+		}
+
+		protected override void OnGUI(){
+			EditorGUILayout.BeginVertical(LayoutSettings.mainBox.style);
+			EditorGUILayout.LabelField("Actor", LayoutSettings.sectionLabel);
+			EditorGUILayout.BeginHorizontal(LayoutSettings.editWidth);
+			EditorGUILayout.ObjectField(actor, typeof(Scripts.Actor), true);
+			if(GUILayout.Button("Refresh", LayoutSettings.buttonOp)){
+				actor = FindObjectOfType<Scripts.Actor>();
+			}
+
+			EditorGUILayout.EndHorizontal();
+			if(actor || Application.isEditor && Application.isPlaying){
+				DrawMethodButton();
+			}
+		}
+
+		private void DrawMethodButton(){
+			ActorTeleport();
+			ActorReceiveReward();
+		}
+
+		private void ActorReceiveReward(){
+			EditorGUILayout.BeginVertical(LayoutSettings.editWidth);
+			var reward = string.Empty;
+			EditorGUILayout.TextField("Reward Message", reward);
+			var isReceived = GUILayout.Button("Receive Reward", LayoutSettings.editWidth);
+			if(isReceived){
+				actor.ReceiveReward(reward);
+			}
+
+			EditorGUILayout.EndVertical();
+		}
+
+		private void ActorTeleport(){
+			EditorGUILayout.BeginVertical(LayoutSettings.editWidth);
+			var teleportPosition = Vector3.zero;
+			EditorGUILayout.Vector3Field("Position", teleportPosition);
+			var isTeleport = GUILayout.Button("Teleport", LayoutSettings.editWidth);
+			if(isTeleport){
+				actor.Teleport(teleportPosition);
+			}
+
+			EditorGUILayout.EndVertical();
+		}
 	}
 }
 #endif
