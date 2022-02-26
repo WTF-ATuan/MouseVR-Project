1f6be6fa0d2bdd12d1bb0ebd5cbde93024ffcced
ATuan <4A8K0069@stust.edu.tw>
2022-02-26 15:02:59 +0800

    can open prefab

--- Assets/Environment/Editor/EditEnvironmentData.cs
+++ Assets/Environment/Editor/EditEnvironmentData.cs
@@ -1,9 +1,18 @@
 ﻿using Environment.Scripts;
 using Sirenix.OdinInspector;
+using UnityEditor;
 
 namespace Environment.Editor{
 	public class EditEnvironmentData{
 		[ShowInInspector] [InlineEditor(Expanded = true)]
 		public EnvironmentData data;
+
+		private bool IsDataNull => data == null;
+
+		[Button(ButtonSizes.Small)]
+		private void OpenDataPrefab(){
+			if(IsDataNull) return;
+			AssetDatabase.OpenAsset(data.mapModel);
+		}
 	}
 }
