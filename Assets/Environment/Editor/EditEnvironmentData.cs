3c986cd7a26790b65f379a8a54dc0a5f14f6d718
ATuan <4A8K0069@stust.edu.tw>
2022-03-07 15:08:40 +0800

    base PathCreator Editor

--- Assets/Environment/Editor/EditEnvironmentData.cs
+++ Assets/Environment/Editor/EditEnvironmentData.cs
@@ -1,18 +1,43 @@
 ﻿using Environment.Scripts;
+using PathCreation;
 using Sirenix.OdinInspector;
 using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
 
 namespace Environment.Editor{
 	public class EditEnvironmentData{
-		[ShowInInspector] [InlineEditor(Expanded = true)]
-		public EnvironmentData data;
-
 		private bool IsDataNull => data == null;
 
+		[HorizontalGroup("Data")] [ShowInInspector] [InlineEditor(Expanded = false)]
+		public EnvironmentData data;
+
+		[HorizontalGroup("Data")]
 		[Button(ButtonSizes.Small)]
-		private void OpenDataPrefab(){
+		private void OpenPrefab(){
 			if(IsDataNull) return;
-			AssetDatabase.OpenAsset(data.mapModel);
+			AssetDatabase.OpenAsset(data.mapPrefab);
+		}
+
+		[HorizontalGroup("path")] [ShowInInspector] [InlineEditor(Expanded = false)]
+		public PathCreator pathCreator;
+
+		[HorizontalGroup("path")]
+		[Button(ButtonSizes.Small)]
+		private void AutoFind(){
+			var mapPrefab = data.mapPrefab;
+			var creator = mapPrefab.GetComponentInChildren<PathCreator>();
+			if(creator != null) pathCreator = creator;
+		}
+
+		[HorizontalGroup("path")]
+		[Button(ButtonSizes.Small)]
+		private void Create(){
+			var gameObject = new GameObject("PathCreator");
+			var creator = gameObject.AddComponent<PathCreator>();
+			StageUtility.PlaceGameObjectInCurrentStage(gameObject);
+			pathCreator = creator;
+
 		}
 	}
 }
--- Assets/Environment/Scripts/EnvironmentData.cs
+++ Assets/Environment/Scripts/EnvironmentData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Environment.Scripts{
 	[CreateAssetMenu(fileName = "EnvironmentData", menuName = "Project/ EnvironmentData")]
@@ -11,8 +12,8 @@ namespace Environment.Scripts{
 		[BoxGroup("Basic Info")] [LabelWidth(100)] [TextArea]
 		public string description;
 
-		[HorizontalGroup("Map Data", 100)] [PreviewField(100)] [HideLabel]
-		public GameObject mapModel;
+		[FormerlySerializedAs("mapModel")] [HorizontalGroup("Map Data", 100)] [PreviewField(100)] [HideLabel]
+		public GameObject mapPrefab;
 
 		[VerticalGroup("Map Data/MapSetting")] [LabelWidth(100)]
 		public List<Characteristic> characteristics;
