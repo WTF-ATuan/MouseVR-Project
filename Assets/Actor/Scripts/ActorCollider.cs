dcb98f373217be91967eafa3b50fc9cd392f0562
ATuan <4A8K0069@stust.edu.tw>
2022-03-20 16:28:02 +0800

    added actorCollider scripts to collision actor

---
+++ Assets/Actor/Scripts/ActorCollider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Actor.Scripts{
+	[RequireComponent(typeof(Actor))]
+	public class ActorCollider : MonoBehaviour{
+		private Actor actor;
+
+		private void Start(){
+			actor = GetComponent<Actor>();
+		}
+
+		private void OnCollisionEnter(Collision other){
+			var otherGameObject = other.gameObject;
+			var actorCollision = otherGameObject.GetComponent<IActorCollision>();
+			actorCollision?.ActorCollision(actor);
+		}
+
+		private void OnTriggerEnter(Collider other){
+			var actorCollision = other.GetComponent<IActorCollision>();
+			actorCollision?.ActorCollision(actor);
+		}
+	}
+	public interface IActorCollision{
+		void ActorCollision(Actor actor);
+	}
+}
