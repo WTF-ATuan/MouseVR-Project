223596e47b5125df469db5f18f09510fca272341
ATuan <4A8K0069@stust.edu.tw>
2022-04-01 18:14:38 +0800

    Extract interface IRotate.cs to Rotate Actor with different way and also can de active rotate

---
+++ Assets/Actor/Scripts/LinearRotate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Actor.Scripts{
+	public class LinearRotate : MonoBehaviour , IRotate{
+		[SerializeField] private float rotateSpeed;
+
+		private new Rigidbody rigidbody;
+
+		private void Start(){
+			rigidbody = GetComponent<Rigidbody>();
+		}
+
+		public void Rotate(bool isRight){
+			var eulerAngel = isRight ? Vector3.up : Vector3.down;
+			var deltaRotation = Quaternion.Euler(eulerAngel * rotateSpeed * Time.fixedDeltaTime);
+			rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+		}
+	}
+}
