cc5996736f844adad59b3138bb2e6741b31e23cf
ATuan <4A8K0069@stust.edu.tw>
2022-04-07 20:32:36 +0800

    show off

--- Assets/Actor/Scripts/ActorTimer.cs
+++ Assets/Actor/Scripts/ActorTimer.cs
@@ -6,37 +6,28 @@ using Project;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class ActorTimer : MonoBehaviour
-{
-    [SerializeField] private float Timer = 0 , LimitTime = 2.5f;
+public class ActorTimer : MonoBehaviour{
+	[SerializeField] private float Timer = 0, LimitTime = 2.5f;
 
-    [SerializeField] private Actor.Scripts.Actor actor;
-    
-    void Start()
-    {
-        EventBus.Subscribe<ActorMoveDetected>(OnActorMoveDetected);
-        actor = GetComponent<Actor.Scripts.Actor>();
-    }
-    
-    [Button]
-    private async void OnActorMoveDetected(ActorMoveDetected obj)
-    {
-        if (Timer > LimitTime)
-        {
-            await Task.Delay(1000);
-            actor.ResetActor();
-            Timer = 0;
-        }
-        else
-        {
-            if (Mathf.Abs(obj.InputSpeed) >= 0.3f)
-            {
-                Timer = 0;
-            }
-            else
-            {
-                Timer += Time.deltaTime;
-            }
-        }
-    }
+	[SerializeField] private Actor.Scripts.Actor actor;
+
+	void Start(){
+		EventBus.Subscribe<ActorMoveDetected>(OnActorMoveDetected);
+		actor = GetComponent<Actor.Scripts.Actor>();
+	}
+
+	private void OnActorMoveDetected(ActorMoveDetected obj){
+		if(Timer > LimitTime){
+			actor.ResetActor();
+			Timer = 0;
+		}
+		else{
+			if(Mathf.Abs(obj.InputSpeed) >= 0.3f){
+				Timer = 0;
+			}
+			else{
+				Timer += Time.deltaTime;
+			}
+		}
+	}
 }
--- Assets/Environment/Scripts/LickTrigger.cs
+++ Assets/Environment/Scripts/LickTrigger.cs
@@ -1,56 +1,58 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using Actor.Scripts.Event;
 using Environment.Scripts.Events;
 using Project;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class LickTrigger : MonoBehaviour
-{
-    [SerializeField] private int correctLickCount, wrongLickCount;
+namespace Environment.Scripts{
+	public class LickTrigger : MonoBehaviour{
+		[SerializeField] [ReadOnly] private int correctLickCount, wrongLickCount;
 
-    [SerializeField] private int correctLickCountLimit, wrongLickCountLimit;
+		[SerializeField] private int correctLickCountLimit, wrongLickCountLimit;
 
-    [SerializeField] private Collider collider;
-    // Start is called before the first frame update
-    void Start()
-    {
-        collider = GetComponent<Collider>();
-        
-        EventBus.Subscribe<ActorLickDetected>(OnActorLickDetected);
-    }
+		private new Collider collider;
 
-    private void OnActorLickDetected(ActorLickDetected obj)
-    {
-        var isContains = collider.bounds.Contains(obj.LickPosition);
+		private void Start(){
+			collider = GetComponent<Collider>();
 
-        if (isContains)
-        {
-            correctLickCount++;
-        }
-        else
-        {
-            wrongLickCount++;
-        }
-        
-        CalculateLickCount();
-    }
+			EventBus.Subscribe<ActorLickDetected>(OnActorLickDetected);
+		}
 
-    private void CalculateLickCount()
-    {
-        if (correctLickCount >= correctLickCountLimit)
-        {
-            EventBus.Post(new ActorJudged(false));
-            correctLickCount = 0;
-            wrongLickCount = 0;
-        }
+		private void OnActorLickDetected(ActorLickDetected obj){
+			var isContains = collider.bounds.Contains(obj.LickPosition);
 
-        if (wrongLickCount >= wrongLickCountLimit)
-        {
-            EventBus.Post(new ActorJudged(true));
-            correctLickCount = 0;
-            wrongLickCount = 0;
-        }
-    }
+			if(isContains){
+				correctLickCount++;
+			}
+			else{
+				wrongLickCount++;
+			}
+
+			CalculateLickCount();
+		}
+
+		private void CalculateLickCount(){
+			if(correctLickCount >= correctLickCountLimit){
+				EventBus.Post(new ActorJudged(false));
+				correctLickCount = 0;
+				wrongLickCount = 0;
+			}
+
+			if(wrongLickCount >= wrongLickCountLimit){
+				EventBus.Post(new ActorJudged(true));
+				correctLickCount = 0;
+				wrongLickCount = 0;
+			}
+		}
+
+		private void OnDrawGizmos(){
+			if(!collider) return;
+			var bounds = collider.bounds;
+			var boundsCenter = bounds.center;
+			var boundsSize = bounds.size;
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireCube(boundsCenter, boundsSize);
+		}
+	}
 }
