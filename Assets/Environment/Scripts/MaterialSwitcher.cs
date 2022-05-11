01a1701aa1d95a73c61e6c1467370a69a5689d4e
ATuan <4A8K0069@stust.edu.tw>
2022-05-12 00:48:21 +0800

    added MaterialSwitcher

---
+++ Assets/Environment/Scripts/MaterialSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Environment.Scripts{
+	public class MaterialSwitcher : MonoBehaviour{
+		[SerializeField] private GameObject targetObject;
+		[SerializeField] private Material targetMaterial;
+		[SerializeField] private int materialIndex;
+		private Material _originMaterial;
+
+		[Button]
+		private void Switch(){
+			if(targetObject == null){
+				var meshRenderer = GetComponent<MeshRenderer>();
+				var currentMaterials = meshRenderer.materials;
+				currentMaterials[materialIndex] = targetMaterial;
+				meshRenderer.materials = currentMaterials;
+			}
+			else{
+				var meshRenderer = targetObject.GetComponent<MeshRenderer>();
+				var currentMaterials = meshRenderer.materials;
+				currentMaterials[materialIndex] = targetMaterial;
+				meshRenderer.materials = currentMaterials;
+			}
+		}
+	}
+}
