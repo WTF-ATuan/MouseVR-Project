5780b7d228e6ecd9bfc2ac13d2a270967e49f278
philippe <philippe8910@gmail.com>
2022-05-18 20:11:59 +0800

    added press M to Switch All Wall material

--- Assets/Environment/Scripts/MaterialSwitcher.cs
+++ Assets/Environment/Scripts/MaterialSwitcher.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Actor.Scripts.Event;
+using Project;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -9,8 +11,27 @@ namespace Environment.Scripts{
 		[SerializeField] private int materialIndex;
 		private Material _originMaterial;
 
+		private void Start()
+		{
+			EventBus.Subscribe<SwitchMaterialDetected>(OnSwitchMaterialDetected);
+		}
+
+		private void OnSwitchMaterialDetected(SwitchMaterialDetected obj)
+		{
+			Switch();
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.M))
+			{
+				EventBus.Post(new SwitchMaterialDetected());
+			}
+		}
+
 		[Button]
 		private void Switch(){
+			
 			if(targetObject == null){
 				var meshRenderer = GetComponent<MeshRenderer>();
 				var currentMaterials = meshRenderer.materials;
