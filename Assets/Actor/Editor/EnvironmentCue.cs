c1f3475aa5573e3de0a2e988015f48841697e669
ATuan <4A8K0069@stust.edu.tw>
2022-06-16 01:38:01 +0800

    editor changed

--- Assets/Actor/Editor/EnvironmentCue.cs
+++ Assets/Actor/Editor/EnvironmentCue.cs
@@ -10,13 +10,10 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Actor.Editor
-{
-	public class EnvironmentCue : OdinEditorWindow
-	{
+namespace Actor.Editor{
+	public class EnvironmentCue : OdinEditorWindow{
 		[MenuItem("Tools/Project/EnvironmentCue")]
-		private static void OpenWindow()
-		{
+		private static void OpenWindow(){
 			var window = GetWindow<EnvironmentCue>();
 			window.position = GUIHelper.GetEditorWindowRect().AlignCenter(500, 500);
 			window.Show();
@@ -28,19 +25,18 @@ namespace Actor.Editor
 		private ArduinoDataReader arduinoDataReader;
 
 		private SerializedObject serializedObject;
-		
-		[SerializeField]
-		private List<Material> currentMaterials = new List<Material>();
+
+		[SerializeField] private List<Material> currentMaterials = new List<Material>();
 
 		private SerializedProperty currentMaterialSerializedObject;
-		
-		[SerializeField]
-		private List<Material> switchtMaterials = new List<Material>();
-		
+
+		[SerializeField] private List<Material> switchtMaterials = new List<Material>();
+
 		private SerializedProperty switchtMaterialsSerializedObject;
 
-		protected override void OnEnable()
-		{
+		private MeshRenderer _meshRenderer;
+
+		protected override void OnEnable(){
 			actor = FindObjectOfType<Scripts.Actor>();
 			settingPanel = FindObjectOfType<SettingPanel>();
 			arduinoBasic = FindObjectOfType<ArduinoBasic>();
@@ -51,40 +47,34 @@ namespace Actor.Editor
 			switchtMaterialsSerializedObject = serializedObject.FindProperty("switchtMaterials");
 		}
 
-		protected override void OnGUI()
-		{
+		protected override void OnGUI(){
 			EditorGUILayout.BeginVertical();
 			EditorGUILayout.BeginHorizontal();
 
+			_meshRenderer = (MeshRenderer)EditorGUILayout.ObjectField(_meshRenderer, typeof(MeshRenderer), true);
 			EditorGUILayout.EndHorizontal();
-			if (actor || Application.isEditor && Application.isPlaying)
-			{
+			if(actor || Application.isEditor && Application.isPlaying){
 				DrawMethodButton();
 			}
 		}
 
-		private void DrawMethodButton()
-		{
+		private void DrawMethodButton(){
 			DashboardUpPos();
 			DashboardDownPos();
 		}
-		
 
 
-		private void DashboardDownPos()
-		{
+		private void DashboardDownPos(){
 			EditorGUILayout.BeginHorizontal();
-			EditorGUILayout.PropertyField(currentMaterialSerializedObject, true);
-			EditorGUILayout.PropertyField(switchtMaterialsSerializedObject, true);
+			EditorGUILayout.PropertyField(currentMaterialSerializedObject, true, GUILayout.Width(200));
+			EditorGUILayout.PropertyField(switchtMaterialsSerializedObject, true, GUILayout.Width(200));
 			EditorGUILayout.EndHorizontal();
-			
-			
+
+
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField("");
-			if (GUILayout.Button("Switch"))
-			{
-				
-			}
+			if(GUILayout.Button("Switch")){ }
+
 			EditorGUILayout.EndHorizontal();
 		}
 
@@ -92,49 +82,43 @@ namespace Actor.Editor
 		private float worldContrast;
 		private float AllCueContrast;
 		private float visibility;
-		
-		private void DashboardUpPos()
-		{
+
+		private void DashboardUpPos(){
 			EditorGUILayout.BeginVertical();
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField("World brightness");
 			worldbrightness = EditorGUILayout.Slider(worldbrightness, 1, 100);
 			EditorGUILayout.EndHorizontal();
 			EditorGUILayout.EndVertical();
-			
+
 			EditorGUILayout.BeginVertical();
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField("World contrast");
 			worldContrast = EditorGUILayout.Slider(worldContrast, 1, 100);
 			EditorGUILayout.EndHorizontal();
 			EditorGUILayout.EndVertical();
-			
+
 			EditorGUILayout.BeginVertical();
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField("All-cue contrast");
 			AllCueContrast = EditorGUILayout.Slider(AllCueContrast, 1, 100);
 			EditorGUILayout.EndHorizontal();
 			EditorGUILayout.EndVertical();
-			
+
 			EditorGUILayout.BeginVertical();
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField("Visibility");
 			visibility = EditorGUILayout.Slider(visibility, 1, 100);
 			EditorGUILayout.EndHorizontal();
 			EditorGUILayout.EndVertical();
-			
-			
-
 		}
 
-		private void DashLine()
-		{
+		private void DashLine(){
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField("----------------------------------------");
 			EditorGUILayout.LabelField("----------------------------------------");
 			EditorGUILayout.LabelField("----------");
 			EditorGUILayout.EndHorizontal();
 		}
-		
 	}
 }
