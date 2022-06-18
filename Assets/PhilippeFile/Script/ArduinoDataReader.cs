86abc85e4324320d9355aca16bfd0c8808da1c33
UsagihimeDenka <philippe8910@gmail.com>
2022-06-18 11:20:14 +0800

    added more data read feature

--- Assets/PhilippeFile/Script/ArduinoDataReader.cs
+++ Assets/PhilippeFile/Script/ArduinoDataReader.cs
@@ -1,31 +1,60 @@
 ﻿using System;
 using System.Linq;
+using UnityEngine;
 
-namespace PhilippeFile.Script{
-	public class ArduinoDataReader{
-		public float[] SplitData;
+namespace PhilippeFile.Script
+{
+    public class ArduinoDataReader
+    {
+        public float[] SplitData;
 
 
-		public void ReadData(string readMessage){
-			var splitDataStr = readMessage.Split(',');
-			SplitData = new float[splitDataStr.Length];
-			ChangeStringToFloat(splitDataStr);
-		}
+        public void ReadData(string readMessage)
+        {
+            var splitDataStr = readMessage.Split(',');
+            SplitData = new float[splitDataStr.Length];
+            ChangeStringToFloat(splitDataStr);
+        }
 
-		private void ChangeStringToFloat(string[] inData){
-			try{
-				for(int i = 0; i < inData.Length; i++){
-					SplitData[i] = float.Parse(inData[i]);
-				}
-			}
-			catch(Exception e){
-				Console.WriteLine(e);
-				throw;
-			}
-		}
+        private void ChangeStringToFloat(string[] inData)
+        {
+            try
+            {
+                for (int i = 0; i < inData.Length; i++)
+                {
+                    SplitData[i] = float.Parse(inData[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
 
-		public float GetSpeed(){
-			return SplitData[2];
-		}
-	}
+        public float GetSpeed()
+        {
+            return SplitData[1];
+        }
+
+        public bool IsLick()
+        {
+            var lickValue = SplitData[3];
+            return lickValue.Equals(1);
+        }
+
+        /// <summary>
+        ///  0 => None
+        ///  1 => Right
+        ///  2 => Left
+        ///  3 => Both
+        /// </summary>
+        /// <returns></returns>
+        public int GetLeverDirection()
+        {
+            var levelValue = SplitData[4];
+            var levelDirectionNumber = Mathf.RoundToInt(levelValue);
+            return levelDirectionNumber;
+        }
+    }
 }
